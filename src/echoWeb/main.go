fb83da4860c3919908981be638138826fabda2d4
JangGeonil <>
2020-06-08 02:29:08 +0900

    Hello Go

---
+++ src/echoWeb/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"scrapper"
+)
+func main() {
+	fmt.Println("S")
+	scrapper.Scrape("s")
+}
---
+++ src/learGo/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"learGo/banking"
+	"fmt"
+)
+
+func main(){
+	account := banking.NewAccount(gi);
+	fmt.Println(account)
+}
